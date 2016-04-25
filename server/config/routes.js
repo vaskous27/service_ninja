1fee29e058986c99a9dfc6326bf7c398d76f0960
Chris Lee <chris.leeau@me.com>
2016-04-25 16:59:39 -0700

    .

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,6 +5,7 @@ var reviews = require('./../controllers/reviews.js');
 
   module.exports = function(app, passport) {
   	app.get('/login', function(req, res) {
+  		        // res.render('index.ejs'); // load the index.ejs file
 
        
     });
@@ -14,4 +15,11 @@ var reviews = require('./../controllers/reviews.js');
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
+
+  	app.get('/signup', function(req, res) {
+  		console.log('signup get');
+        // render the page and pass in any flash data if it exists
+        // res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
   };
