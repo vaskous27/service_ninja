444da8df35eee4a1f5a42fc775609fa904360309
Chris Lee <chris.leeau@me.com>
2016-04-25 16:21:21 -0700

    .

--- server.js
+++ server.js
@@ -6,20 +6,24 @@ var passport = require('passport');
 var cookieParser = require('cookie-parser');
 var session      = require('express-session');
 var bodyParser = require('body-parser'); 
-require('./server/config/mongoose.js');
-require('./server/config/routes.js')(app, passport);
+
 
 var app = express();
 app.use(bodyParser.json());
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch', resave: true,
+    saveUninitialized: true })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // set up a static file server that points to the "client" directory
 app.use(express.static(path.join(__dirname, '/client')));
+require('./server/config/mongoose.js');
+require('./server/config/routes.js')(app, passport);
+require('./server/config/passport')(passport); // pass passport for configuration
+
 app.listen(8000, function() {
   console.log('cool stuff on: 8000');
 });
--- server/config/routes.js
+++ server/config/routes.js
@@ -3,6 +3,15 @@ var users = require('./../controllers/users.js');
 var reviews = require('./../controllers/reviews.js');  
 
 
-  module.exports = function(app) {
-    
+  module.exports = function(app, passport) {
+  	app.get('/login', function(req, res) {
+
+       
+    });
+
+  	app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
   };
