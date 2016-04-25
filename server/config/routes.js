d49587f640d426dda175e784c60d46cfa825edbe
at0misk <ktp925@gmail.com>
2016-04-25 15:11:23 -0700

    Updated Jobs Partial

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,16 +5,16 @@ var friends = require('./../controllers/friends.js');
   // We will have to require this in the server.js file (and pass it app!)
   module.exports = function(app) {
   // verb: get, plural of target as the URI is the RESTful index method (it returns all friends)
-	app.get('/friends', function(req, res) {
-	  friends.index(req, res);
-	})
+	// app.get('/friends', function(req, res) {
+	//   friends.index(req, res);
+	// })
 
-  app.post('/friends', function(req, res) {
-    friends.post(req, res);
-  })
-	// note how we are delegating to the controller and passing along req and res
-  app.get('/delete/:id', function(req, res) {
-  	friends.remove(req, res, req.params.id);
-  })
+ //  app.post('/friends', function(req, res) {
+ //    friends.post(req, res);
+ //  })
+	// // note how we are delegating to the controller and passing along req and res
+ //  app.get('/delete/:id', function(req, res) {
+ //  	friends.remove(req, res, req.params.id);
+ //  })
 
   };
