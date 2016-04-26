758edc682db9d5c7d9eeeb83b1d6e8e657a285d0
at0misk <ktp925@gmail.com>
2016-04-26 14:30:17 -0700

    Updated partials and functionality

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,18 @@
-
-var users = require('./../controllers/users.js'); 
-var reviews = require('./../controllers/reviews.js');  
-  module.exports = function(app) {
-    
+var users = require('./../controllers/users.js');
+var services = require('./../controllers/services.js');  
+var reviews = require('./../controllers/reviews.js'); 
+var categories = require('./../controllers/categories.js');  
+  module.exports = function(app){
+    app.get('/services', function(req, res){
+      services.index(req, res);
+    });
+    app.post('/services', function(req, res){
+      services.create(req, res);
+    });
+    app.get('/services/:id', function(req, res){
+      services.find(req, res);
+    });
+    app.get('/categories', function(req, res){
+      categories.index(req, res);
+    });
   };
