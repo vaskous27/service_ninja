66a64464c5e9abce523ce84cf086905505cc7f2d
Chris Lee <chris.leeau@me.com>
2016-04-26 12:55:14 -0700

    .

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,24 +1,26 @@
-
 var users = require('./../controllers/users.js'); 
 var reviews = require('./../controllers/reviews.js');  
+var passport = require('passport');
+
+module.exports = function(app) {
+
+	app.get("/", function(req, res) {
+		res.render("index");
+	})
+
+	app.get("/auth/facebook", passport.authenticate("facebook"));
+
+	app.get("/auth/facebook/callback", passport.authenticate("facebook", { failureRedirect: "/" }), function(req, res) {
+		res.render("success", { user: req.session.passport.user });
+	});
+
+	app.get("/auth/google", passport.authenticate("google", { scope: "https://www.googleapis.com/auth/plus.login" }));
+
+	app.get("/auth/google/callback", passport.authenticate("google", { failureRedirect: "/" }), function(req, res) {
+		res.render("profile", { user: req.session.passport.user });
+		// res.json(json_data);
+		// res.sendfile('../../client/partials/profile.html')
+	});
+}
 
 
-
-  module.exports = function(app, passport) {
-  	app.get('/login', function(req, res) {
-  		        // res.render('index.ejs'); // load the index.ejs file
-
-       
-    });
-
-  	app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/profile', // redirect to the secure profile section
-        failureRedirect : '/signup', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
-
-  	app.get('/signup', function(req, res) {
-  		console.log('signup get');
-        // render the page and pass in any flash data if it exists
-        // res.render('signup.ejs', { message: req.flash('signupMessage') });
-    });
--- server/models/user.js
+++ server/models/user.js
@@ -1,39 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
+ 
+// Thanks to http://blog.matoski.com/articles/jwt-express-node-mongoose/
+ 
+// set up a mongoose model
 var UserSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  skills: String,
-  location: String,
-  categories: String,
-  reviews: [{type: Schema.Types.ObjectId, ref: 'Review'}],
-  local            : {
-        email        : String,
-        password     : String,
-    },
-    facebook         : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    },
-    google           : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    }
+    authId: Number,
+    name: String,
+    provider: String,
+    json_info: Object
 });
-
-// methods ======================
-// generating a hash
-UserSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// checking if password is valid
-UserSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
-
-var User = mongoose.model('User', UserSchema);
+ 
+module.exports = mongoose.model('User', UserSchema);
