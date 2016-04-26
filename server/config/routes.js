584d9206d298331dfaf5916e6694a3a20c1d9751
at0misk <ktp925@gmail.com>
2016-04-26 14:44:33 -0700

    Fixed some stupid bugs

--- server/config/routes.js
+++ server/config/routes.js
@@ -7,11 +7,11 @@ var categories = require('./../controllers/categories.js');
     app.get('/login', function(req, res) {
               // res.render('index.ejs'); // load the index.ejs file 
     });
-    app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/profile', // redirect to the secure profile section
-        failureRedirect : '/signup', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
+    // app.post('/signup', passport.authenticate('local-signup', {
+    //     successRedirect : '/profile', // redirect to the secure profile section
+    //     failureRedirect : '/signup', // redirect back to the signup page if there is an error
+    //     failureFlash : true // allow flash messages
+    // }));
     app.get('/signup', function(req, res) {
       console.log('signup get');
         // render the page and pass in any flash data if it exists
